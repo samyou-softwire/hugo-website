91bc1fd73c3102f20b9348329d3b015cd346b87f
Samuel <samuel.young@softwire.com>
2024-06-10 13:43:15 +0100

    add music

--- assets/js/konami.js
+++ assets/js/konami.js
@@ -5,6 +5,11 @@ document.addEventListener("keydown", (event => {
     keys = keys.slice(1);
     
     if (JSON.stringify(keys) === JSON.stringify(["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight"])) {
-        console.log("konami!!")
+        var sound = new Howl({
+            src: ['https://files.catbox.moe/df4h75.mp3'],
+            html5: true
+        });
+        
+        sound.play();
     }
 }));
